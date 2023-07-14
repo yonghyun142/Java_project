c48bff1b1b8d7c928e7e01f0df53cc08fc140b97
DESKTOP-K7F5CVG\kim_Y.H <adam1234kr@naver.com>
2023-07-14 18:11:40 +0900

    수정

---
+++ src/project1/DAO.java
@@ -0,0 +1,9 @@
+package project1;
+
+
+import java.util.Scanner;
+
+public class DAO { //게시판 기능
+	Scanner scanner = new Scanner(System.in);
+	List<DAO>
+}
---
+++ src/project1/VO.java
@@ -0,0 +1,40 @@
+package project1;
+
+public class VO {
+	private int num;
+	private String title;
+	private String content;
+	private String writer;
+	private String registDate;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getRegistDate() {
+		return registDate;
+	}
+	public void setRegistDate(String registDate) {
+		this.registDate = registDate;
+	}
+}
